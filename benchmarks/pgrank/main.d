8d2666aa098c8a1f55510d0de137d0f755ba2c1b
Gurneet <gurneetkaur2@gmail.com>
2025-05-22 23:01:08 -0700

    removing extra files

--- benchmarks/pgrank/main.d
+++
@@ -1,6 +0,0 @@
-main.o: main.cpp data.pb.h ../../engine/mapreduce.hpp \
- ../../engine/mapreduce.h ../../engine/util.h \
- ../../engine/infinimem/fileIO.h ../../engine/infinimem/util.h \
- ../../engine/mapwriter.h ../../engine/mapwriter.hpp \
- ../../engine/infinimem/twoPhaseFileIO.hpp \
- ../../engine/infinimem/fileIO.h
